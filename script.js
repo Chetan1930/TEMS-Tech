2c7fffba29e010733af1b9fc3d7275247c873a23
Chetan Chauhan <devilmafia71@gmail.com>
2025-02-21 17:27:12 +0530

    majorly changes done

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function orderNow() {
     alert('Thank you for your order!');
 }
+
+
+new Swiper('.testimonial-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});

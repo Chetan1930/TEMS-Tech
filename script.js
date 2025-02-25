21c99096538dad514592a36873035c28ea81ed4a
Chetan Chauhan <devilmafia71@gmail.com>
2025-02-25 08:02:24 +0530

    working of hamburger

--- script.js
+++ script.js
@@ -31,3 +31,13 @@ new Swiper('.testimonial-slider', {
         },
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
